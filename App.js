ac175862a13861bf0a2d0940df07f766c56a74cf
Luis Meza <2402060@utrivieramaya.edu.mx>
2026-04-06 22:10:33 -0500

    build(starProject): este es el inicio del proyecto subiendolo a gitHub

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>esta es la actividad que estamos desarrollando entre oliver, moises y yo (luis)</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10
+  },
+});
