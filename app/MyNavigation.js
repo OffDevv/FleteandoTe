2a10bde2a7d8c23d0d5e8dff0e68157a2dd5e152
Luis Meza <2402060@utrivieramaya.edu.mx>
2026-04-06 23:37:10 -0500

    build(startProject): agregacion de librerias

---
+++ app/MyNavigation.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function MyNavigation() {
+  return (
+    <View>
+      <Text>MyNavigation</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
